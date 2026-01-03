b3f81433074bd91a9b6fe3b12cdfdb4a3532bb77
Sneha Bagli <147941875+snehabagli@users.noreply.github.com>
2026-01-03 13:07:33 +0530

    Update Signup.jsx

--- creative-showcase/client/src/pages/Signup.jsx
+++ creative-showcase/client/src/pages/Signup.jsx
@@ -11,7 +11,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://creative-showcase-fuw5.onrender.com/auth/register", {
+      const res = await fetch("https://creative-showcase-fuw5.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
